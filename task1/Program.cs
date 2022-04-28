3e1f67e5c3cbe0fa970ca801d3c80d24056936c3
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-28 10:47:55 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        delegate double MyDelegate(double r);
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            MyDelegate myDelegate = Circumference;
+            double d = myDelegate(r);
+            Console.Write($"\nДлинна окружности равна: {d:F2}");
+
+            myDelegate = AreaOfACircle;
+            double s = myDelegate(r);
+            Console.Write($"\nПлощадь круга равна: {s:F3}");
+
+            myDelegate = BallVolume;
+            double v = myDelegate(r);
+            Console.Write($"\nОбъём круга равен: {v:F4}");
+            Console.ReadLine();
+
+        }
+
+        static double Circumference(double r) => 2 * Math.PI * r;
+        static double AreaOfACircle(double r) => Math.PI * Math.Pow(r, 2);
+        static double BallVolume(double r) => 4 / 3 * Math.PI * Math.Pow(r, 3);
+
+    }
+}
